3f9baf3eac8c642d0205ac8b4c381adfd238b353
DavidXLiu <dxl4041@rit.edu>
2017-10-27 23:20:29 -0400

    Tried Shader Shit

---
+++ HeroJam Bio-Robots/Assets/Scripts/Shader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineShader : MonoBehaviour {
+
+    public Shader shader;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseOver()
+    {
+        gameObject.GetComponent<Renderer>().material.shader = shader;
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<Renderer>().material.shader = Shader.Find("Diffuse");
+    }
+}
